820a47fe2a8049ee6b2a2016983798ce99256021
ssamya <mikhaildrum01@gmail.com>
2025-03-24 19:20:01 +0200

    create orders app and orders.models

--- users/views.py
+++ users/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
+from django.contrib import auth
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from .forms import UserLoginForm
 
 
 def login(request):
-    return render(request, 'users/login.html')
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username,
+                                     password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('main:product_list'))
+    else:
+        form = UserLoginForm()
+    return render(request, 'users/login.html', {'form': form})
 
 
 def registration(request):
